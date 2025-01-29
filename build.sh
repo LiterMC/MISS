bb68d56c8d2e8d37c4c929aede8da39f75f08689
zyxkad <zyxkad@gmail.com>
2025-01-28 17:09:16 -0700

    fix wrong path

--- build.sh
+++ build.sh
@@ -10,5 +10,5 @@ rm fabric/build/libs/* neoforge/build/libs/*
 mkdir output
 echo "Copying files"
 cp fabric/build/libs/* output/
-cp neoforge/build/libs/* output/
+cp forge/build/libs/* output/
 echo "File copied, exit code: " $?
